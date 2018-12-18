2eb9f732d17bd81d8a8f918c7fed61a0fdc803f1
聂杞锋 <308561157@qq.com>
2018-12-18 11:50:06 +0800

    add nfeng-validate

--- js/background.js
+++ js/background.js
@@ -13,12 +13,25 @@ chrome.runtime.onInstalled.addListener(function() {
 });
 
 chrome.contextMenus.create({
-	title: "测试右键菜单",
-	onclick: function() { alert('您点击了右键菜单！'); }
+	title: '谷歌搜索：%s', // %s表示选中的文字
+	contexts: ['selection'], // 只有当选中文字时才会出现此右键菜单
+	onclick: function(params) {
+		// 注意不能使用location.href，因为location是属于background的window对象
+		chrome.tabs.create({ url: 'https://www.google.com.tw/search?q=' + encodeURI(params.selectionText) });
+	}
 });
 
 chrome.contextMenus.create({
-	title: '使用度娘搜索：%s', // %s表示选中的文字
+	title: '度娘搜索：%s', // %s表示选中的文字
+	contexts: ['selection'], // 只有当选中文字时才会出现此右键菜单
+	onclick: function(params) {
+		// 注意不能使用location.href，因为location是属于background的window对象
+		chrome.tabs.create({ url: 'https://www.baidu.com/s?ie=utf-8&wd=' + encodeURI(params.selectionText) });
+	}
+});
+
+chrome.contextMenus.create({
+	title: '度娘翻译：%s', // %s表示选中的文字
 	contexts: ['selection'], // 只有当选中文字时才会出现此右键菜单
 	onclick: function(params) {
 		// 注意不能使用location.href，因为location是属于background的window对象
--- js/inject.js
+++ js/inject.js
@@ -22,10 +22,30 @@ function sendMessageToContentScriptByPostMessage(data) {
 })();
 
 window.devtoolsFormatters = [{
-    header: function(obj){
-        return ['div', {}, `${JSON.stringify(obj, null, 7)}`]
+    header: function(obj) {
+        if (!obj.__clown) {
+            return null;
+        }
+        delete obj.__clown;
+        const style = `
+        color: red;
+        border: dotted 2px gray;
+        border-radius: 4px;
+        padding: 5px;
+      `
+        const content = `🤡 ${JSON.stringify(obj, null, 2)}`;
+
+        try {
+            return ['div', { style }, content]
+        } catch (err) { // for circular structures
+            return null;  // use the default formatter
+        }
     },
-    hasBody: function(){
+    hasBody: function() {
         return false;
     }
 }]
+
+console.clown = function(obj) {
+    console.log({ ...obj, __clown: true });
+}
